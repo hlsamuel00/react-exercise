36f1557e4295fdabbbbb0176bb2e22c968e9abd7
Harvey S <hlsamuel00@yahoo.com>
2022-07-08 15:42:26 -0400

    add search feature to phonebook

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -17,14 +17,15 @@ const PhoneBook = ({persons}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '123-156-2245'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const[search, setSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -53,17 +54,24 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    setSearch(event.target.value)
+  }
+
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(search)) || persons
+
   return (
     <div>
         <h2>PhoneBook</h2>
+        <div>filter shown with <input value={search} onChange={handleSearch} /></div>
+        <h2>add a new</h2>
         <form onSubmit={addPerson} >
           <div> name: <input value={newName} onChange={handleNameChange} /></div>
           <div> number: <input value={newNumber} onChange={handleNumberChange} /></div>
           <div> <button type='submit'>add</button> </div>
         </form>
         <h2>Numbers</h2>
-        <PhoneBook persons={persons} />
-        <div>debug: {newName}</div>
+        <PhoneBook persons={personsToShow} />
     </div>
   )
 }
