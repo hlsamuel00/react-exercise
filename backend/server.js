a3e2176a4b435cd6c6f2b66d9bf3438c9087c6a3
Harvey S <hlsamuel00@yahoo.com>
2022-07-27 13:33:49 -0400

    add new file and functionality to backend

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+
+app.use(express.json())
+
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+      },
+      { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+      },
+      { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+      },
+      { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+      }
+]
+
+app.get('/info', (req,res) => {
+    res.send(`<p>The Phonebook contains ${persons.length} entries.</p><p>${new Date()}`)
+})
+
+app.get('/api/persons', (req,res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const person = persons.find(person => person.id === parseInt(req.params.id))
+
+    person ? res.json(person) : res.status(404).json({error: 'Invalid Entry'})
+})
+
+const PORT = 8888
+app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}; you better go catch it!!`)
+})
